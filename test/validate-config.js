1ae67fa6be12695bed2281b502c6c3dacb84fb98
Laurent Blanes <laurent.blanes@gmail.com>
2017-02-04 02:22:42 +0900

    Adapted Standard library to my needs

--- test/validate-config.js
+++ test/validate-config.js
@@ -9,7 +9,7 @@ test('load config in eslint to validate all rule syntax is correct', function (t
     configFile: 'eslintrc.json'
   })
 
-  var code = 'var foo = 1\nvar bar = function () {}\nbar(foo)\n'
+  var code = 'var foo = 1;\nvar bar = function () {};\nbar(foo);\n'
 
   t.equal(cli.executeOnText(code).errorCount, 0)
   t.end()
